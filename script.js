dc22506416a1619d412573f6f6e13738d32bceb4
Gabriel Oliveira <105285297+eoGabrielo@users.noreply.github.com>
2022-11-20 19:42:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const usuario = document.querySelector("#usuario");
+const senha = document.querySelector("#senha");
+
+const mostrarUsuario = document.querySelector("#mostrarUsuario");
+const mostrarSenha = document.querySelector("#mostrarSenha");
+
+const botao = document.querySelector("#botao");
+const botaoApagar = document.querySelector("#botaoApagar");
+
+function apagarInfo() {
+    mostrarUsuario.innerHTML = " ";
+    mostrarSenha.innerHTML = " ";
+}
+
+function mostrarResultados() {
+
+    if (usuario.value.length > 3 && senha.value.length >= 7) {
+        mostrarUsuario.innerHTML = usuario.value;
+        mostrarSenha.innerHTML = senha.value;
+        let nomeFormatado = usuario.value;
+        alert("Seja bem vindo(a) " + nomeFormatado[0].toUpperCase() + nomeFormatado.substring(1));
+    } else {
+        const mostrarUsuario = document.querySelector("#mostrarUsuario");
+        mostrarUsuario.innerHTML = "Usuario precisa ter mais de 3 digitos";
+        const mostrarSenha = document.querySelector("#mostrarSenha");
+        mostrarSenha.innerHTML = "Senha precisa ter mais de 7 digitos"
+        alert("Siga as instruções, que serão informadas");
+    }
+}
+
+
+botao.addEventListener("click", mostrarResultados);
+botaoApagar.addEventListener("click", apagarInfo);
+
